14d209526c16bc02e34f9dad9ae64fddad8b5bd5
leshka290 <lleesshhkkaa00@gmail.com>
2023-07-20 20:51:43 +0300

    change table users_data_table

--- src/main/java/pro/sky/telegrambot/model/User.java
+++ src/main/java/pro/sky/telegrambot/model/User.java
@@ -4,11 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.sql.Timestamp;
 
-@Entity(name = "usersDataTable")
+@Entity(name = "users_data_table")
 @Getter
 @Setter
 public class User {
